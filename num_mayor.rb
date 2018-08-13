10a45478f83cfb40cb9e9d927670f012e446b69d
Dreft12 <jorgeelieceraldana.92@gmail.com>
2018-08-13 18:47:41 -0500

    Agregar Numero Mayor Ruby

---
+++ num_mayor.rb
@@ -0,0 +1,11 @@
+class NumMayor
+  puts "Ingrese primer numero"
+  num_1 = gets.to_i
+  puts "Ingrese segundo numero"
+  num_2 = gets.to_i
+  if num_1 > num_2
+    puts "El numero 1 es mayor que el numero 2"
+  else
+    puts "El numero 2 es mayor que el numero 1"
+  end
+end
